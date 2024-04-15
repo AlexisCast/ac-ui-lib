9f8b187c2536888d566dfd4bb46e8bddcbe32492
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-04-15 05:31:07 -0600

    made configurations to be able to use the components in other react apps with tailwind/css/module css

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,20 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended'
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+    'react/react-in-jsx-scope': 'off'
+  }
+};
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,54 +1,39 @@
-import React from "react";
-import "./button.css";
+import React from 'react';
+import classNames from 'classnames';
+import styles from './button.module.css';
 
 interface ButtonProps {
-	/**
-	 * Is this the principal call to action on the page?
-	 */
-	primary?: boolean;
-	/**
-	 * What background color to use
-	 */
-	backgroundColor?: string;
-	/**
-	 * How large should the button be?
-	 */
-	size?: "small" | "medium" | "large";
-	/**
-	 * Button contents
-	 */
-	label: string;
-	/**
-	 * Optional click handler
-	 */
-	onClick?: () => void;
+  variant?: 'default' | 'primary' | 'secondary';
+  backgroundColor?: string;
+  size?: 'sm' | 'md' | 'lg';
+  label: string;
+  onClick?: () => void;
+  className?: string;
 }
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({
-	primary = false,
-	size = "medium",
-	backgroundColor,
-	label,
-	...props
+  variant = 'default',
+  size = 'md',
+  backgroundColor,
+  label,
+  className,
+  ...props
 }: ButtonProps) => {
-	const mode = primary
-		? "storybook-button--primary"
-		: "storybook-button--secondary";
-	return (
-		<button
-			type="button"
-			className={[
-				"storybook-button",
-				`storybook-button--${size}`,
-				mode,
-			].join(" ")}
-			style={{ backgroundColor }}
-			{...props}
-		>
-			{label}
-		</button>
-	);
+  return (
+    <button
+      type="button"
+      className={classNames(
+        styles.button,
+        styles[`button--${variant}`],
+        styles[`button--${size}`],
+        className
+      )}
+      style={{ backgroundColor }}
+      {...props}>
+      {label}
+    </button>
+  );
 };
